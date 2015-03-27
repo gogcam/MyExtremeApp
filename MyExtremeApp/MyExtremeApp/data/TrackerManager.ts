184383566ce664a9d4911553458dbc9f419908a5
gogcam <silvan.kurth@bluewin.ch>
2015-03-27 09:19:26 +0100

    fdsfds

--- MyExtremeApp/MyExtremeApp/data/TrackerManager.ts
+++ MyExtremeApp/MyExtremeApp/data/TrackerManager.ts
@@ -1,4 +1,6 @@
-﻿interface loadResponse {
+﻿/// <reference path="db.ts" />
+
+interface loadResponse {
     TrackerList: TrackerType[];
     StatusCode: number;
     StatusMessage: string;
@@ -8,19 +10,18 @@ class TrackerManager {
 
     public _csTrackers: DevExpress.data.CustomStore;
     public _dbOptions: DevExpress.data.CustomStoreOptions;
-    private SERVICE_URL_GET_TRACKERS: string = 'http://gpswebservice.sorba.ch/GPSWebService/Sys/rest/GetTrackers/%7B%22KundenNr%22:%2236373%22,%22Domain%22:%22KMUmitKST14%22%7D'
-    private SERVICE_URL_UPDATE_TRACKER: string = 'http://gpswebservice.sorba.ch/GPSWebService/Sys/rest/UpdateTracker'
-    private SERVICE_URL_GET_GPSSYSTEMS: string = 'http://gpswebservice.sorba.ch/GPSWebService/Sys/rest/GetGPSSystems'
-    //private SERVICE_URL_GET_TRACKERS: string = 'http://devsk-nb1-win81/GPSWebService/Sys/rest/GetTrackers/%7B%22KundenNr%22:%2236373%22,%22Domain%22:%22KMUmitKST14%22%7D'
-    //private SERVICE_URL_UPDATE_TRACKER: string = 'http://devsk-nb1-win81/GPSWebService/Sys/rest/UpdateTracker'
-    //private SERVICE_URL_GET_GPSSYSTEMS: string = 'http://devsk-nb1-win81/GPSWebService/Sys/rest/GetGPSSystems'
+    //private SERVICE_URL_GET_TRACKERS: string = 'http://gpswebservice.sorba.ch/GPSWebService/Sys/rest/GetTrackers/%7B%22KundenNr%22:%2236373%22,%22Domain%22:%22KMUmitKST14%22%7D'
+    //private SERVICE_URL_UPDATE_TRACKER: string = 'http://gpswebservice.sorba.ch/GPSWebService/Sys/rest/UpdateTracker'
+    //private SERVICE_URL_GET_GPSSYSTEMS: string = 'http://gpswebservice.sorba.ch/GPSWebService/Sys/rest/GetGPSSystems'
+    private SERVICE_URL_GET_TRACKERS: string = 'http://devsk-nb1-win81/GPSWebService/Sys/rest/GetTrackers/%7B%22KundenNr%22:%2236373%22,%22Domain%22:%22KMUmitKST14%22%7D'
+    private SERVICE_URL_UPDATE_TRACKER: string = 'http://devsk-nb1-win81/GPSWebService/Sys/rest/UpdateTracker'
+    private SERVICE_URL_GET_GPSSYSTEMS: string = 'http://devsk-nb1-win81/GPSWebService/Sys/rest/GetGPSSystems'
 
     constructor() {
         this.initTrackersCustomStoreSettings();
     }
 
     private initTrackersCustomStoreSettings(): void {
-        var self = this;
         this._dbOptions = {
             load: (loadOptions) => {
                 return this.SendRequestLoad(this.SERVICE_URL_GET_TRACKERS, 'GET');
@@ -31,7 +32,14 @@ class TrackerManager {
             },
             insert: (params) => {
                 //postJson(SERVICE_URL_UPDATE_TRACKER, params, undefined, undefined);
-                return this.SendRequest(this.SERVICE_URL_UPDATE_TRACKER, 'POST', params);
+                var mydb = new db.db();
+                var json_data = JSON.stringify(params);
+                json_data = '{"Tracker":' + json_data + '}';
+                var deferred: JQueryDeferred<any> = mydb.SendRequest_POST(this.SERVICE_URL_UPDATE_TRACKER, json_data);
+
+                return deferred;
+
+                //return this.SendRequest(this.SERVICE_URL_UPDATE_TRACKER, 'POST', params);
             },
             //remove: (params) => {
             //    //return dbImpl._sendRequest('DELETE', params);
---
+++ MyExtremeApp/MyExtremeApp/data/db.ts
@@ -0,0 +1,58 @@
+﻿module db {
+    export class db {
+
+        constructor() {
+
+        }
+
+        public SendRequest_GET(url: string, json_data?: string): any {
+            var deferred: JQueryDeferred<void> = jQuery.Deferred<void>();
+
+            var requestSettings: any = {
+                url: $.trim(url),
+                type: 'GET',
+                success: function (data) {
+                    var x = data;
+                    deferred.resolve(data);
+                }
+            };
+
+            //if (params) {
+            //    var json_data = JSON.stringify(params);
+            //    json_data = '{"Tracker":' + json_data + '}';
+
+            //    requestSettings.contentType = 'application/json';
+            //    requestSettings.dataType = "Text";
+            //    requestSettings.data = json_data;
+            //}
+
+            //$.ajax(requestSettings);
+
+            return deferred;
+        }
+
+        public SendRequest_POST(url: string, json_data?: string): JQueryDeferred<any> {
+            var deferred: JQueryDeferred<any> = jQuery.Deferred<any>();
+
+            var requestSettings: any = {
+                url: $.trim(url),
+                type: 'POST',
+                success: function (data: loadResponse) {
+                    var x = data;
+                    deferred.resolve(data);
+                }
+            };
+
+            if (json_data) {
+                requestSettings.contentType = 'application/json';
+                requestSettings.dataType = "Text";
+                requestSettings.data = json_data;
+            }
+
+            $.ajax(requestSettings);
+
+            return deferred;
+        }
+
+    }
+}
